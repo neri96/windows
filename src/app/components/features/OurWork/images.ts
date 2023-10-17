8058a71786fbc36ffe6d3ead97e708be64aad927
babwc <admin@babushkamarket.com>
2023-10-16 22:44:20 -0700

    added images

--- src/app/components/features/OurWork/images.ts
+++ src/app/components/features/OurWork/images.ts
@@ -5,18 +5,24 @@ import img2 from "@/app/assets/images/gallery/img2.jpg";
 import img3 from "@/app/assets/images/gallery/img3.jpg";
 import img4 from "@/app/assets/images/gallery/img4.jpg";
 import img5 from "@/app/assets/images/gallery/img5.jpg";
-// import img9 from "@/app/assets/images/gallery/img9.jpg";
-import img10 from "@/app/assets/images/gallery/img10.webp";
+import img7 from "@/app/assets/images/gallery/img7.jpg";
+// import img9 from "@/app/assets/images/gallery/img9.JPG";
 import img11 from "@/app/assets/images/gallery/img11.jpg";
 import img13 from "@/app/assets/images/gallery/img13.jpg";
 import img14 from "@/app/assets/images/gallery/img14.jpg";
-import img16 from "@/app/assets/images/gallery/img16.webp";
-import img21 from "@/app/assets/images/gallery/img21.webp";
-// import img22 from "@/app/assets/images/gallery/img22.jpg";
-// import img34 from "@/app/assets/images/gallery/img34.jpg";
-// import img35 from "@/app/assets/images/gallery/img35.jpg";
+import img15 from "@/app/assets/images/gallery/img15.jpg";
 
 export const images = [
+  {
+    id: uuid(),
+    title: "Marvin windows installation",
+    img: img1,
+  },
+  {
+    id: uuid(),
+    title: "Provia fibreglass entry door",
+    img: img2,
+  },
   {
     id: uuid(),
     title: "Aluminium windows",
@@ -32,26 +38,17 @@ export const images = [
     title: "Aluminium windows and doors 2",
     img: img5,
   },
+
   {
     id: uuid(),
-    title: "Provia fibreglass entry door",
-    img: img2,
-  },
-  {
-    id: uuid(),
-    title: "Marvin windows installation",
-    img: img1,
+    title: "  Anlin vinyl windows and patio doors",
+    img: img7,
   },
   // {
   //   id: uuid(),
   //   title: "Door new construction installation, exterior finish redwood",
   //   img: img9,
   // },
-  {
-    id: uuid(),
-    title: "Door. Before and after installation",
-    img: img10,
-  },
   {
     id: uuid(),
     title: "Bay window",
@@ -69,32 +66,7 @@ export const images = [
   },
   {
     id: uuid(),
-    title: "New Opening",
-    img: img16,
+    title: "Fibreglass Milgard Ultra windows and doors",
+    img: img15,
   },
-  {
-    id: uuid(),
-    title: "Milgard vinyl windows. Before and after",
-    img: img21,
-  },
-  // {
-  //   id: uuid(),
-  //   title: "Milgard vinyl windows. Before and after",
-  //   img: img21,
-  // },
-  // {
-  //   id: uuid(),
-  //   title: "New construction installation",
-  //   img: img22,
-  // },
-  // {
-  //   id: uuid(),
-  //   title: "New construction installation + stucco work",
-  //   img: img34,
-  // },
-  // {
-  //   id: uuid(),
-  //   title: "New construction installation + stucco work 2",
-  //   img: img35,
-  // },
 ];
