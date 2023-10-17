f8c5a2ff19ee008183abd7809b769a734edb0954
babwc <admin@babushkamarket.com>
2023-10-17 00:17:12 -0700

    images fix

--- src/app/components/features/OurWork/images.ts
+++ src/app/components/features/OurWork/images.ts
@@ -3,12 +3,12 @@ import { v4 as uuid } from "uuid";
 import img1 from "@/app/assets/images/gallery/img1.jpg";
 import img2 from "@/app/assets/images/gallery/img2.jpg";
 import img3 from "@/app/assets/images/gallery/img3.jpg";
-import img4 from "@/app/assets/images/gallery/img4.jpg";
-import img5 from "@/app/assets/images/gallery/img5.jpg";
-import img7 from "@/app/assets/images/gallery/img7.jpg";
+// import img4 from "@/app/assets/images/gallery/img4.jpg";
+// import img5 from "@/app/assets/images/gallery/img5.jpg";
+// import img7 from "@/app/assets/images/gallery/img7.jpg";
 // import img9 from "@/app/assets/images/gallery/img9.JPG";
-import img11 from "@/app/assets/images/gallery/img11.jpg";
-import img13 from "@/app/assets/images/gallery/img13.jpg";
+// import img11 from "@/app/assets/images/gallery/img11.jpg";
+// import img13 from "@/app/assets/images/gallery/img13.jpg";
 // import img14 from "@/app/assets/images/gallery/img14.jpg";
 // import img15 from "@/app/assets/images/gallery/img15.jpg";
 
@@ -18,32 +18,32 @@ export const images = [
     title: "Marvin windows installation",
     img: img1,
   },
-  // {
-  //   id: uuid(),
-  //   title: "Provia fibreglass entry door",
-  //   img: img2,
-  // },
+  {
+    id: uuid(),
+    title: "Provia fibreglass entry door",
+    img: img2,
+  },
   {
     id: uuid(),
     title: "Aluminium windows",
     img: img3,
   },
-  {
-    id: uuid(),
-    title: "Aluminium windows and doors",
-    img: img4,
-  },
-  {
-    id: uuid(),
-    title: "Aluminium windows and doors 2",
-    img: img5,
-  },
+  // {
+  //   id: uuid(),
+  //   title: "Aluminium windows and doors",
+  //   img: img4,
+  // },
+  // {
+  //   id: uuid(),
+  //   title: "Aluminium windows and doors 2",
+  //   img: img5,
+  // },
 
-  {
-    id: uuid(),
-    title: "  Anlin vinyl windows and patio doors",
-    img: img7,
-  },
+  // {
+  //   id: uuid(),
+  //   title: "  Anlin vinyl windows and patio doors",
+  //   img: img7,
+  // },
   // {
   //   id: uuid(),
   //   title: "Door new construction installation, exterior finish redwood",
@@ -54,11 +54,11 @@ export const images = [
   //   title: "Bay window",
   //   img: img11,
   // },
-  {
-    id: uuid(),
-    title: "Bay window 2",
-    img: img13,
-  },
+  // {
+  //   id: uuid(),
+  //   title: "Bay window 2",
+  //   img: img13,
+  // },
   // {
   //   id: uuid(),
   //   title: "Fibreglass Milgard Ultra windows",
