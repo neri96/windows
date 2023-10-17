dd1eecf528b754e7f60941479cc089ec71d9ad57
babwc <admin@babushkamarket.com>
2023-10-17 00:23:42 -0700

    animation fix

--- src/app/components/features/OurWork/components/OurWorkCarousel.tsx
+++ src/app/components/features/OurWork/components/OurWorkCarousel.tsx
@@ -39,76 +39,76 @@ const OurWorkCarousel = ({
   };
 
   return (
-    <AnimatePresence>
-      {carouselOpen && (
-        <motion.div
-          className={styles.ourWorkGallery__backdrop}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 0.3 }}
+    <motion.div
+      className={styles.ourWorkGallery__backdrop}
+      variants={{
+        show: { opacity: 1, zIndex: 100 },
+        hidden: { opacity: 0, zIndex: -1 },
+      }}
+      initial="hidden"
+      animate={carouselOpen ? "show" : "hidden"}
+      exit="hidden"
+      transition={{ duration: 0.3 }}
+    >
+      <div className={styles.ourWorkGallery__carousel}>
+        <div
+          className={styles.ourWorkGallery__close}
+          onClick={() => setCarouselOpen(false)}
         >
-          <div className={styles.ourWorkGallery__carousel}>
-            <div
-              className={styles.ourWorkGallery__close}
-              onClick={() => setCarouselOpen(false)}
-            >
-              <Image priority={true} src={CloseIcon} alt="Close" />
-            </div>
-            {index !== 0 ? (
-              <div
-                className={styles.ourWorkGallery__controllerLeft}
-                onClick={handleIndexMinus}
-              >
-                <Image priority={true} src={ArrowIcon} alt="Left" />
-              </div>
-            ) : null}
-            {index !== images.length - 1 ? (
-              <div
-                className={styles.ourWorkGallery__controllerRight}
-                onClick={handleIndexPlus}
-              >
-                <Image priority={true} src={ArrowIcon} alt="Right" />
-              </div>
-            ) : null}
-            <div
-              className={styles.ourWorkGallery__carousel__content}
-              style={{
-                width: `${carouselWidth}%`,
-                transform: `translateX(-${(100 / images.length) * index}%)`,
-              }}
-            >
-              {images.map(
-                ({
-                  id,
-                  title,
-                  img,
-                }: {
-                  id: string;
-                  title: string;
-                  img: StaticImageData;
-                }) => {
-                  return (
-                    <div
-                      key={id}
-                      className={styles.ourWorkGallery__carousel__imgWrap}
-                    >
-                      <h2>{title}</h2>
-                      <Image
-                        className={styles.ourWorkGallery__carousel__img}
-                        src={img}
-                        alt={title}
-                      />
-                    </div>
-                  );
-                }
-              )}
-            </div>
-            <OurWorkPagination currentIndex={index} setIndex={setIndex} />
+          <Image priority={true} src={CloseIcon} alt="Close" />
+        </div>
+        {index !== 0 ? (
+          <div
+            className={styles.ourWorkGallery__controllerLeft}
+            onClick={handleIndexMinus}
+          >
+            <Image priority={true} src={ArrowIcon} alt="Left" />
           </div>
-        </motion.div>
-      )}
-    </AnimatePresence>
+        ) : null}
+        {index !== images.length - 1 ? (
+          <div
+            className={styles.ourWorkGallery__controllerRight}
+            onClick={handleIndexPlus}
+          >
+            <Image priority={true} src={ArrowIcon} alt="Right" />
+          </div>
+        ) : null}
+        <div
+          className={styles.ourWorkGallery__carousel__content}
+          style={{
+            width: `${carouselWidth}%`,
+            transform: `translateX(-${(100 / images.length) * index}%)`,
+          }}
+        >
+          {images.map(
+            ({
+              id,
+              title,
+              img,
+            }: {
+              id: string;
+              title: string;
+              img: StaticImageData;
+            }) => {
+              return (
+                <div
+                  key={id}
+                  className={styles.ourWorkGallery__carousel__imgWrap}
+                >
+                  <h2>{title}</h2>
+                  <Image
+                    className={styles.ourWorkGallery__carousel__img}
+                    src={img}
+                    alt={title}
+                  />
+                </div>
+              );
+            }
+          )}
+        </div>
+        <OurWorkPagination currentIndex={index} setIndex={setIndex} />
+      </div>
+    </motion.div>
   );
 };
 
