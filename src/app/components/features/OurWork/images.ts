a294d72ecd236e1fe3fcbe0b817959359e634799
babwc <admin@babushkamarket.com>
2023-10-16 21:55:31 -0700

    added images

--- src/app/components/features/OurWork/images.ts
+++ src/app/components/features/OurWork/images.ts
@@ -5,43 +5,16 @@ import img2 from "@/app/assets/images/gallery/img2.jpg";
 import img3 from "@/app/assets/images/gallery/img3.jpg";
 import img4 from "@/app/assets/images/gallery/img4.jpg";
 import img5 from "@/app/assets/images/gallery/img5.jpg";
-// import img6 from "@/app/assets/images/gallery/img6.jpg";
-// import img7 from "@/app/assets/images/gallery/img7.jpg";
-// import img8 from "@/app/assets/images/gallery/img8.jpg";
 import img9 from "@/app/assets/images/gallery/img9.jpg";
 import img10 from "@/app/assets/images/gallery/img10.webp";
 import img11 from "@/app/assets/images/gallery/img11.jpg";
-// import img12 from "@/app/assets/images/gallery/img12.jpg";
 import img13 from "@/app/assets/images/gallery/img13.jpg";
 import img14 from "@/app/assets/images/gallery/img14.jpg";
-// import img15 from "@/app/assets/images/gallery/img15.jpg";
 import img16 from "@/app/assets/images/gallery/img16.webp";
-// import img17 from "@/app/assets/images/gallery/img17.jpg";
-// import img18 from "@/app/assets/images/gallery/img18.jpg";
-// import img19 from "@/app/assets/images/gallery/img19.jpg";
-// import img20 from "@/app/assets/images/gallery/img20.jpg";
 import img21 from "@/app/assets/images/gallery/img21.webp";
 import img22 from "@/app/assets/images/gallery/img22.jpg";
-// import img23 from "@/app/assets/images/gallery/img23.jpg";
-// import img24 from "@/app/assets/images/gallery/img24.jpg";
-// import img25 from "@/app/assets/images/gallery/img25.jpg";
-// import img26 from "@/app/assets/images/gallery/img26.jpg";
-// import img27 from "@/app/assets/images/gallery/img27.png";
-// import img28 from "@/app/assets/images/gallery/img28.png";
-// import img29 from "@/app/assets/images/gallery/img29.jpg";
-// import img30 from "@/app/assets/images/gallery/img30.jpg";
-// import img31 from "@/app/assets/images/gallery/img31.jpg";
-// import img32 from "@/app/assets/images/gallery/img32.jpg";
-// import img33 from "@/app/assets/images/gallery/img33.jpg";
 import img34 from "@/app/assets/images/gallery/img34.jpg";
 import img35 from "@/app/assets/images/gallery/img35.jpg";
-// import img36 from "@/app/assets/images/gallery/img36.jpg";
-// import img37 from "@/app/assets/images/gallery/img37.webp";
-// import img38 from "@/app/assets/images/gallery/img38.jpg";
-// import img39 from "@/app/assets/images/gallery/img39.webp";
-// import img40 from "@/app/assets/images/gallery/img40.webp";
-// import img41 from "@/app/assets/images/gallery/img41.jpg";
-// import img42 from "@/app/assets/images/gallery/img42.jpg";
 
 export const images = [
   {
@@ -69,21 +42,6 @@ export const images = [
     title: "Marvin windows installation",
     img: img1,
   },
-  // {
-  //   id: uuid(),
-  //   title: "Aluminium windows and doors 2",
-  //   img: img6,
-  // },
-  // {
-  //   id: uuid(),
-  //   title: "Anlin vinyl windows and patio doors",
-  //   img: img7,
-  // },
-  // {
-  //   id: uuid(),
-  //   title: "Bay window",
-  //   img: img8,
-  // },
   {
     id: uuid(),
     title: "Door new construction installation, exterior finish redwood",
@@ -99,11 +57,6 @@ export const images = [
     title: "Bay window",
     img: img11,
   },
-  // {
-  //   id: uuid(),
-  //   title: "Bay window 2",
-  //   img: img12,
-  // },
   {
     id: uuid(),
     title: "Bay window 2",
@@ -114,11 +67,6 @@ export const images = [
     title: "Fibreglass Milgard Ultra windows",
     img: img14,
   },
-  // {
-  //   id: uuid(),
-  //   title: "Fibreglass Milgard Ultra windows and doors",
-  //   img: img15,
-  // },
   {
     id: uuid(),
     title: "New Opening",
@@ -139,71 +87,6 @@ export const images = [
     title: "New construction installation",
     img: img22,
   },
-  // {
-  //   id: uuid(),
-  //   title: "Milgard single hung windows installed 2",
-  //   img: img23,
-  // },
-  // {
-  //   id: uuid(),
-  //   title: "Marvin windows new construction installation",
-  //   img: img17,
-  // },
-  // {
-  //   id: uuid(),
-  //   title: "Marvin windows new construction installation 2",
-  //   img: img18,
-  // },
-  // {
-  //   id: uuid(),
-  //   title: "Marvin windows new construction installation 3",
-  //   img: img19,
-  // },
-  // {
-  //   id: uuid(),
-  //   title: "Marvin windows new construction installation 4",
-  //   img: img20,
-  // },
-  // {
-  //   id: uuid(),
-  //   title: "Milgard single hung bronze Color windows installed",
-  //   img: img24,
-  // },
-  // {
-  //   id: uuid(),
-  //   title: "Vinyl garden window",
-  //   img: img25,
-  // },
-  // {
-  //   id: uuid(),
-  //   title: "Marvin wood clad windows",
-  //   img: img30,
-  // },
-  // {
-  //   id: uuid(),
-  //   title: "Marvin wood clad windows 2",
-  //   img: img31,
-  // },
-  // {
-  //   id: uuid(),
-  //   title: "Marvin wood clad windows",
-  //   img: img30,
-  // },
-  // {
-  //   id: uuid(),
-  //   title: "Marvin wood clad windows 2",
-  //   img: img31,
-  // },
-  // {
-  //   id: uuid(),
-  //   title: "New construction installation",
-  //   img: img32,
-  // },
-  // {
-  //   id: uuid(),
-  //   title: "New construction installation 2",
-  //   img: img33,
-  // },
   {
     id: uuid(),
     title: "New construction installation + stucco work",
@@ -214,59 +97,4 @@ export const images = [
     title: "New construction installation + stucco work 2",
     img: img35,
   },
-  // {
-  //   id: uuid(),
-  //   title: "New construction installation + stucco work 3",
-  //   img: img36,
-  // },
-  // {
-  //   id: uuid(),
-  //   title: "Retrofit installation. Before and after installation",
-  //   img: img37,
-  // },
-  // {
-  //   id: uuid(),
-  //   title: "Entry fibreglass door",
-  //   img: img26,
-  // },
-  // {
-  //   id: uuid(),
-  //   title: "Entry fibreglass door 2",
-  //   img: img27,
-  // },
-  // {
-  //   id: uuid(),
-  //   title: "Entry fibreglass door 3",
-  //   img: img28,
-  // },
-  // {
-  //   id: uuid(),
-  //   title: "Entry fibreglass door 3",
-  //   img: img29,
-  // },
-  // {
-  //   id: uuid(),
-  //   title: "Provia fibreglass entry door 2",
-  //   img: img38,
-  // },
-  // {
-  //   id: uuid(),
-  //   title: "Sliding door. Before and after installation",
-  //   img: img39,
-  // },
-  // {
-  //   id: uuid(),
-  //   title: "Sliding door. Before and after installation 2",
-  //   img: img40,
-  // },
-  // {
-  //   id: uuid(),
-  //   title: "Simonton vinyl windows and fiberglass Therma Tru door",
-  //   img: img41,
-  // },
-  // {
-  //   id: uuid(),
-  //   title: "Simonton vinyl windows and fiberglass Therma Tru door 2",
-  //   img: img42,
-  // },
 ];
